b9b46778b834ab1a616452b28d1b339483b9c1bf
LensCrafters3 <168283064+LensCrafters3@users.noreply.github.com>
2024-05-04 11:35:37 -0400

    added updated script

--- script.js
+++ script.js
@@ -117,3 +117,89 @@ function closeWindow(window){
     window.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+function addToCart(id, image, name, price) {
+    /* get product info store in array */
+    let items = [id, image, name, price];
+
+    /* get data from localstorage */
+    let from_store = localStorage.getItem("shopping-cart");
+
+    /* if localstore is not null 
+    The ternary operator is a conditional operator which evaluates either of two expressions 
+    – a true expression and a false expression
+     – based on a conditional expression that you provide.
+
+     syntax
+     condition ? trueExpression : falseExpression
+     condition == from_store
+     ifTrue == JSON.parse(from_store)
+     ifFalse == [] 
+     assign whatever result to tmp variable
+    */
+    let tmp = from_store ? JSON.parse(from_store) : [];
+
+    /* add new array to current array */
+    tmp.push(items);
+
+    console.log(tmp);
+
+    /* assign to localstorage */
+    localStorage.setItem("shopping-cart", JSON.stringify(tmp));
+    /* set alert that item was added to cart */
+    //alert("Added to Cart");
+}
+
+
+function showCart() {
+    /* retrieve from localstorage,save in variable */
+    let cart = JSON.parse(localStorage.getItem("shopping-cart"));
+    /* empty variable to store formatted items */
+    let all_items = "";
+
+    /* use a for loopw to go through the retrieve localstorage array and
+    display the items in a formatted way */
+    for (var i = 0; i < cart.length; i++) {
+        all_items +=
+            "<div class=\"listing2\"><img src=\""
+            + cart[i][1] + "\" style=\"width:150px; padding: 20px;\"><h4 style=\"padding: 20px;\">"
+            + cart[i][2] + "</h4><p style=\"padding: 20px;\">"
+            + cart[i][3] + "</p>"
+            + "<button class=\"remove\" onclick=\"removeItem(" + cart[i][0] + ")\">Remove</button></div>";
+        document.getElementById("cart-items").innerHTML = all_items;
+    }
+}
+function removeItem(arg) {
+    /* get items from localstorage and save to a locl variable */
+    let cart = JSON.parse(localStorage.getItem("shopping-cart"));
+    /* create a blank variable that will store new, filtered array */
+    let filteredUsers = [];
+
+    for (let i = 0; i < cart.length; i++) {
+        /* if the id of the item in the cart is not the same as the item we are looking for
+        which is whatever the value of arg is then we add that item to the new filtered array */
+        if (cart[i][0] != arg) {
+            filteredUsers = [...filteredUsers, cart[i]];
+        }
+    }
+    /* add the new filtered array to the localstorage */
+    localStorage.setItem("shopping-cart", JSON.stringify(filteredUsers));
+    /* reload the page to show new cart items */
+    window.location.reload();
+}
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.targtet.matches("[data-dropdown-button]")
+    if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let activeDropdown
+    if (isDropdownButton) {
+        activeDropdown = e.target.closest('[data-dropdown]')
+        activeDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown-button].active").forEach(dropdown => {
+        if (dropdown === activeDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
