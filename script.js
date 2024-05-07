6e50553227dcbefe1f0f202686621378a465d008
LensCrafters3 <168283064+LensCrafters3@users.noreply.github.com>
2024-05-07 12:24:50 -0400

    script language for back to top button

--- script.js
+++ script.js
@@ -203,3 +203,22 @@ document.addEventListener('click', e => {
         dropdown.classList.remove('active')
     })
 })
+
+var backToTopbutton = document.getElementById('back-to-top-btn');
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.scrollY > 327) {
+        backToTopbutton.style.display = "block";
+    }
+    else {
+        backToTopbutton.style.display = "none"
+    }
+}
+
+backToTopbutton.addEventListener("click", backToTop);
+
+function backToTop(){
+    window.scrollTo(0, 0);
+}
